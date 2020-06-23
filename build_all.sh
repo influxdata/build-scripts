181e70c0925466e7b297c19bf78b9e3c76fb0b13
Anthony Murphy <anthony.murphy@themadbotter.com>
2020-06-23 13:30:00 -0400

    specified ubuntu as target dist on build_all

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Declare an array of string with type
 #("ubuntu" "i386/ubuntu:eoan-20200410" "arm32v7/ubuntu:eoan-20200410" "centos" "arm64v8/ubuntu:eoan-20200608")
-declare -a TargetsArray=("arm64v8/ubuntu:eoan-20200608")
+declare -a TargetsArray=("ubuntu")
 echo "Checking Docker installation"
 if ! [ -x "$(command -v docker)" ]; then
   echo 'Error: docker is not installed. Install docker first. try "sudo apt install docker.io" or equivalent' >&2
